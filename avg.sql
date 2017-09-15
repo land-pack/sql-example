04ad89852708acd57ad55c75f79aba9fbae2c6ba
landpack <ada.frank.ak@gmail.com>
2017-09-15 09:33:43 +0800

    fix

--- avg.sql
+++ avg.sql
@@ -11,8 +11,15 @@ WHERE f_orderstatus=1 AND f_platform='android' AND f_crtime between "2017-04-01"
 GROUP BY f_money
 
 
-#查询平均日充值数据
+#query avg 
 SELECT f_money, count(*) as counter, TIMESTAMPDIFF(DAY,"2017-04-01", "2017-09-14") as days, sum(f_money) as moneys ,count(*)/TIMESTAMPDIFF(DAY,"2017-04-01", "2017-09-14") as average_counter,  sum(f_money)/TIMESTAMPDIFF(DAY,"2017-04-01", "2017-09-14") as average_money
 FROM t_diamond_recharge
 WHERE f_orderstatus=1 AND f_platform='android' AND f_crtime between "2017-04-01" AND "2017-09-14"
 GROUP BY f_money
+
+
+#query user place
+SELECT f_uid, sum(f_golds) as golds
+FROM t_gold_log
+WHERE f_crtime between "2017-04-01" AND "2017-09-14" AND f_inout in (1, 14)
+GROUP BY f_uid
